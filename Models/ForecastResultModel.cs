469a26eeea441cdcbb1dfa94fb24523f591f1cc4
Rickard von Haugwitz <rickardvh@gmail.com>
2022-01-26 15:12:47 +0100

    fix(all): request new results per page, refactor

--- Models/ForecastResultModel.cs
+++ Models/ForecastResultModel.cs
@@ -1,20 +1,8 @@
-using System;
-using System.Collections.Generic;
-using Nop.Web.Areas.Admin.Models.Catalog;
-using Nop.Web.Areas.Admin.Factories;
-using Nop.Core.Infrastructure;
 using Nop.Web.Framework.Models;
-using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Majako.Plugin.Misc.SalesForecasting.Models
 {
-    public class ForecastResultModel : BaseSearchModel
-    {
-        public string ResultsJson { get; set; }
-
-        public ForecastResultModel()
-        {
-            ResultsJson = "[]";
-        }
-    }
+  public class ForecastResultModel : BaseSearchModel
+  {
+  }
 }
